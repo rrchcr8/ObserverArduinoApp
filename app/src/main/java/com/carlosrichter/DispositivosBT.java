b54f8794bbe1edbb01bebee8be55515f7a71872d
Rrch <rrch.cr8@gmail.com>
2020-01-28 01:06:19 -0400

    Initial commit

---
+++ app/src/main/java/com/carlosrichter/DispositivosBT.java
@@ -0,0 +1,14 @@
+package com.carlosrichter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DispositivosBT extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dispositivos_bt);
+    }
+}
---
+++ app/src/main/java/com/carlosrichter/UserInterfaz.java
@@ -0,0 +1,14 @@
+package com.carlosrichter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UserInterfaz extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_interfaz);
+    }
+}
