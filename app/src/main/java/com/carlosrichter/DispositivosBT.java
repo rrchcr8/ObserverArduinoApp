b76bbfc25b32ce85ca6129c7d0368ad809a7a098
Carlos Richter <carlos.richter@jalasoft.com>
2020-01-29 19:43:30 -0400

    WFW dispositivo BT finished

--- app/src/main/java/com/carlosrichter/DispositivosBT.java
+++ app/src/main/java/com/carlosrichter/DispositivosBT.java
@@ -4,9 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Set;
 
@@ -34,7 +39,7 @@ public class DispositivosBT extends AppCompatActivity {
         //Presentalosdispositivos vinculados en el ListView
         IdLista = (ListView) findViewById(R.id.IdLista);
         IdLista.setAdapter(mPairedDevicesArrayAdapater);
-        IdLista.setOnClickListener(mDeviceClickListener);
+        IdLista.setOnItemClickListener(mDeviceClickListener);
         //Obtiene el adaptador localBluetooth Adapter
         mBtAdapter = BluetoothAdapter.getDefaultAdapter();
         Set<BluetoothDevice> pairedDevices = mBtAdapter.getBondedDevices();
@@ -46,7 +51,38 @@ public class DispositivosBT extends AppCompatActivity {
         }
     }
 
+    // configura un (on-click) para la lista
 
+    private AdapterView.OnItemClickListener mDeviceClickListener = new AdapterView.OnItemClickListener() {
+        public void onItemClick(AdapterView av, View v, int arg2, long arg3) {
+            //Obtener la direccion MAC del dispositivo, que son los ultimos 17 caracteres en la vista
+            String info = ((TextView) v).getText().toString();
+            String address = info.substring(info.length()-17);
+
+            Intent i = new Intent(DispositivosBT.this, UserInterfaz.class);
+            i.putExtra(EXTRA_DEVICES_ADDRES,address);
+            startActivity(i);
+        }
+    };
+
+    //----------------------------------------------------------------------------------------------
+
+    //Metodo para que el dispositivo BT esta encendido y que el dispositivo movil soporte el servicio
+    private void VerificarEstadoBT(){
+        //Comprueba que el dispositivo tiene Bluetooth y que esta encendido
+        mBtAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (mBtAdapter == null){
+            Toast.makeText(getBaseContext(), "El dispositivo no soporta Bluetooth", Toast.LENGTH_SHORT).show();
+        }else{
+            if (mBtAdapter.isEnabled()){
+                Toast.makeText(getBaseContext(), "...Bluetooth Activado...", Toast.LENGTH_SHORT).show();
+            }else{
+                //Solicita al usuario que active Bluetooth
+                Intent enableBTIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableBTIntent,1);
+            }
+        }
+    }
 
 
 }
