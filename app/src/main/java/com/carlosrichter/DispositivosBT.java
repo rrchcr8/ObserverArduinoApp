f47aaf888739b17856185e611239cc92251831af
Rrch <rrch.cr8@gmail.com>
2020-01-29 03:11:58 -0400

    implementarion

--- app/src/main/java/com/carlosrichter/DispositivosBT.java
+++ app/src/main/java/com/carlosrichter/DispositivosBT.java
@@ -2,15 +2,32 @@ package com.carlosrichter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.bluetooth.BluetoothAdapter;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class DispositivosBT extends AppCompatActivity {
+
+    private static final String TAG = "DispositivosBT";
+    ListView IdLista;
+    public  static String EXTRA_DEVICES_ADDRES = "device_address";
+
+    private BluetoothAdapter mBtAdapter;
+    private ArrayAdapter<String> mPairedDevicesArrayAdapater;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dispositivos_bt);
+    }
+    @Override
+    public void onResume()
+    {
+        super.onResume();
 
+    mPairedDevicesArrayAdapater = new ArrayAdapter<String>(this, R.layout.nombre_dispositivos);
 
     }
 }
--- app/src/main/java/com/carlosrichter/UserInterfaz.java
+++ app/src/main/java/com/carlosrichter/UserInterfaz.java
@@ -9,20 +9,20 @@ import android.widget.TextView;
 
 public class UserInterfaz extends AppCompatActivity {
 
-    Button IdEncender_aux, IdApagar, IdDesconectar;
+    Button IdEncender, IdApagar, IdDesconectar;
     TextView IdBufferIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_interfaz);
-        IdEncender_aux = (Button) findViewById(R.id.IdEncender);
+        IdEncender = (Button) findViewById(R.id.IdEncender);
         IdApagar = (Button) findViewById(R.id.IdApagar);
         IdDesconectar = (Button) findViewById(R.id.IdDesconectar);
         IdBufferIn = (TextView) findViewById(R.id.IDBufferIn);
 
 
-        IdEncender_aux.setOnClickListener(new View.OnClickListener() {
+        IdEncender.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
