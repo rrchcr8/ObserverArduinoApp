02d20dc45e2b4acac06c9613a5b954e147b3bf2a
Carlos Richter <carlos.richter@jalasoft.com>
2020-01-29 19:14:42 -0400

    WFW

--- app/src/main/java/com/carlosrichter/DispositivosBT.java
+++ app/src/main/java/com/carlosrichter/DispositivosBT.java
@@ -3,9 +3,12 @@ package com.carlosrichter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import java.util.Set;
 
 public class DispositivosBT extends AppCompatActivity {
 
@@ -26,8 +29,24 @@ public class DispositivosBT extends AppCompatActivity {
     public void onResume()
     {
         super.onResume();
-
-    mPairedDevicesArrayAdapater = new ArrayAdapter<String>(this, R.layout.nombre_dispositivos);
-
+        //inicializa el array que contendra la lista de dispositivos bluetooth vinculados
+        mPairedDevicesArrayAdapater = new ArrayAdapter<String>(this, R.layout.nombre_dispositivos);
+        //Presentalosdispositivos vinculados en el ListView
+        IdLista = (ListView) findViewById(R.id.IdLista);
+        IdLista.setAdapter(mPairedDevicesArrayAdapater);
+        IdLista.setOnClickListener(mDeviceClickListener);
+        //Obtiene el adaptador localBluetooth Adapter
+        mBtAdapter = BluetoothAdapter.getDefaultAdapter();
+        Set<BluetoothDevice> pairedDevices = mBtAdapter.getBondedDevices();
+        //adciona un dispositivoprevio  emparejado al Array
+        if(pairedDevices.size()>0){
+            for(BluetoothDevice device : pairedDevices){
+                mPairedDevicesArrayAdapater.add(device.getName()+"\n"+device.getAddress());
+            }
+        }
     }
+
+
+
+
 }
