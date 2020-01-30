627f0adeb7c607c662b15b2bec089a8532db2b32
Rrch <rrch.cr8@gmail.com>
2020-01-29 23:24:26 -0400

    bluetoothIn implemented

--- app/src/main/java/com/carlosrichter/UserInterfaz.java
+++ app/src/main/java/com/carlosrichter/UserInterfaz.java
@@ -2,6 +2,7 @@ package com.carlosrichter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothSocket;
 import android.os.Bundle;
@@ -9,6 +10,8 @@ import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import java.util.UUID;
 
 public class UserInterfaz extends AppCompatActivity {
 
@@ -21,8 +24,14 @@ public class UserInterfaz extends AppCompatActivity {
     private BluetoothSocket btSocket = null;
     private StringBuilder DataStringIN = new StringBuilder();
     private ConnectedThread MyConexionBT;
+    //identificador unico de servicio - SPP UUID
+    private static final UUID BTMODULEUUID = UUID.fromString("00001101-0000-1000-800-00805F9B34FB");
+    //declaraciond e variable string apra la direccion MAc
+    private  static String address = null;
+    //----------------------------------
 
 
+    @SuppressLint("HandlerLeak")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,6 +40,24 @@ public class UserInterfaz extends AppCompatActivity {
         IdApagar = (Button) findViewById(R.id.IdApagar);
         IdDesconectar = (Button) findViewById(R.id.IdDesconectar);
         IdBufferIn = (TextView) findViewById(R.id.IDBufferIn);
+
+        bluetoothIn = new Handler(){
+            public void handleMessage(android.os.Message msg) {
+                if (msg.what == handlerState) {
+                    String readMessage = (String) msg.obj;
+                    DataStringIN.append(readMessage);
+
+                    int endOfLineIndex = DataStringIN.indexOf("#");
+                    if (endOfLineIndex > 0){
+                        String dataPrint = DataStringIN.substring(0, endOfLineIndex);
+                        IdBufferIn.setText("Dato:" + dataPrint);
+                        DataStringIN.delete(0,DataStringIN.length());
+                    }
+                }
+            }};
+
+
+
 
 
         IdEncender.setOnClickListener(new View.OnClickListener() {
