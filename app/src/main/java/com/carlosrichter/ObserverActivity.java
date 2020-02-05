0c23949c0eba2a84d24a2c1efe99fedec0c638f4
Carlos Richter <carlos.richter@jalasoft.com>
2020-02-05 17:47:46 -0400

    Added UI elements

--- app/src/main/java/com/carlosrichter/ObserverActivity.java
+++ app/src/main/java/com/carlosrichter/ObserverActivity.java
@@ -1,14 +1,23 @@
 package com.carlosrichter;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ObserverActivity extends AppCompatActivity {
 
+    private TextView textView;
+    private Button button;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_observer);
+        textView = (TextView)findViewById(R.id.txt_obs);
+        button = (Button) findViewById(R.id.btn_obs);
+
+
     }
 }
